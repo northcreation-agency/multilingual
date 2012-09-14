b822da598b52f441755814140728e768ad9c010f
Herbert Cuba Garcia <herbert@kreationsbyran.se>
2012-09-14 18:28:23 +0200

    Updated the Task to activate a language for all pages & dataobject at once!

--- code/ActivateLanguageTask.php
+++ code/ActivateLanguageTask.php
@@ -7,19 +7,19 @@ class ActivateLanguageTask extends BuildTask {
 	protected $enabled = true;
 
 	function run($request) {
-		$this->ActivatLang();
+		$this->ActivateLang();
 		
 		DataObject::flush_and_destroy_cache();
 	}
 	function ActivateLang(){
 		$idArray = explode('&', $_SERVER["QUERY_STRING"]);		
-
-		if (sizeof($idArray) > 1) {
+		array_shift($idArray);		
+		if (sizeof($idArray) > 0) {
 			foreach ($idArray as $ignore => $avPair) {
-				list($index, $value) = explode("=", $avPair);
-				$allowed_langs = array_keys(self::map_locale());
-				print_r($allowed_langs);
-				if ($value && in_array($index, $allowed_langs)) {
+				list($index, $value) = explode("=", $avPair);				
+				$allowed_langs = array_keys(Multilingual::map_locale());				
+				$task=$value?"enabled":"disabled";
+				if (in_array($index, $allowed_langs)) {
 					if ($index == Multilingual::default_lang()) {
 						$fieldname = "LangActive";
 					} else {
@@ -33,24 +33,27 @@ class ActivateLanguageTask extends BuildTask {
 					foreach ($results as $obj) {
 						if (!empty($obj["ClassName"])) {
 							$object = DataObject::get_by_id($obj["ClassName"], $obj["ID"]);
-							$object->$fieldname = true;
+							$object->$fieldname = $value;
 							$object->write();
+							DB::alteration_message("DataObject#".$object->ID." (".$object->MenuTitle.") -  <strong>".$index."</strong> ".$task,"changed");
 						}
 					}
 
-
+					
 					//fix pages
 					$sqlquery = new SQLQuery("*", "MultilingualPage");
 					$results = $sqlquery->execute();
+					
 
-					foreach ($results as $obj) {
+					foreach ($results as $obj) {						
 						$object = DataObject::get_by_id("MultilingualPage", $obj["ID"]);
-						$object->$fieldname = true;
+						$object->$fieldname = $value;
 						if ($object->isPublished()) {
 							$object->doPublish();
 						} else {
 							$object->write();
 						}
+						DB::alteration_message("Page#".$object->ID." (".$object->MenuTitle.") - <strong>".$index."</strong> ".$task,"changed");
 					}
 				}
 			}
